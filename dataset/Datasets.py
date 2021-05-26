9e861da595c0ae49fe3001910962fef27de5d9d5
wonkicho <chowk1109@naver.com>
2021-05-26 21:58:22 +0900

    commit

--- dataset/Datasets.py
+++ dataset/Datasets.py
@@ -1,16 +1,25 @@
+import os
 import torch
 import torch.nn as nn
-import torch.utils.data.dataloader
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 import cv2
 import numpy as np
 
-class CustomDataset:
-    def __init__(self, img_path, transforms = None):
-        self.img_path = img_path
+class CustomDataset(Dataset):
+    def __init__(self, img_path, target,transforms = None, mode = None):
+        self.img_path = os.path.join(img_path, mode)
         self.transforms = transforms
+        self.target = target
 
     def __len__(self):
-        pass
+        return len(self.img_path)
 
     def __getitem__(self, index):
-        
+        image = cv2.imread(self.img_path[index])
+        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        image = self.transforms(image = image)["image"]
+        image = np.transpose(image, (2,0,1)).astype(np.float32)
+
+        target = self.target[index]
+        return {"image" : torch.tensor(image), "target" : torch.tensor(target)} 
