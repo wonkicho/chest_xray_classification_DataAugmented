aa6687f048442afe9f9aa8f975568ab77cc8d5a4
wonkicho <chowk1109@naver.com>
2021-05-26 21:42:47 +0900

    commit

--- dataset/Datasets.py
+++ dataset/Datasets.py
@@ -1,6 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.utils.data.dataloader
+import cv2
+import numpy as np
 
 class CustomDataset:
-    def __init__(self, img_path,)
+    def __init__(self, img_path, transforms = None):
+        self.img_path = img_path
+        self.transforms = transforms
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        
