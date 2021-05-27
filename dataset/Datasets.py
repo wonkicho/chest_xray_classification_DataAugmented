ce2bd43bac3e1dd81b11fcd0b72d51903cc9d451
chowk1109 <chowk1109@naver.com>
2021-05-27 10:34:27 +0900

    commit

--- dataset/Datasets.py
+++ dataset/Datasets.py
@@ -1,16 +1,21 @@
 import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-import cv2
-import numpy as np
+
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
+
 from munge_data import mungeFile
 
 class CustomDataset(Dataset):
-    def __init__(self, img_path, target, transforms = None):
+    def __init__(self, img_path, df, target, transforms = None):
         self.img_path = img_path
+        self.dataframe = df
         self.target = target
         self.transforms = transforms
         
@@ -19,9 +24,11 @@ class CustomDataset(Dataset):
         return len(self.img_path)
 
     def __getitem__(self, index):
-        image = cv2.imread(self.img_path[index])
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        image = self.transforms(image = image)["image"]
+        image_path = os.path.join(self.img_path, str(self.dataframe["image_name"][index]))
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        augmented = self.transforms(image = image)
+        image = augmented["image"]
         image = np.transpose(image, (2,0,1)).astype(np.float32)
 
         targets = self.target[index]
@@ -38,12 +45,15 @@ if __name__ == "__main__":
         return A.Compose(
             [
                 A.Resize(height=256, width=256, p=1),
-                ToTensorV2(p=1.0),
+                #ToTensorV2(p=1.0),
             ], 
-            p=1.0, 
-        )
+            p=1.0)
 
-    img_path = "C://Datasets/chest_xray/train/"
+    img_path = "C:\\Datasets\\chest_xray\\train"
     train_df = mungeFile(img_path)
-    dataset = CustomDataset(img_path=img_path, target = train_df['target'], transforms=get_train_transforms)
-    print(dataset)
+    dataset = CustomDataset(img_path=img_path, df = train_df, target = train_df['target'], transforms=get_train_transforms())
+    data_loader = DataLoader(dataset, shuffle=True, num_workers=0)
+    image = next(iter(data_loader))["image"]
+    image = image.squeeze()
+    
+
--- dataset/munge_data.py
+++ dataset/munge_data.py
@@ -8,6 +8,7 @@ def mungeFile(img_path):
     for cls_idx, img_list in enumerate(image_file):
         for img in img_list:
             total_path = os.path.join(classes[cls_idx], img)
+            #total_path = img_path[:-5] + ".jpg"
             image_path_list.append(total_path)
 
     #target value
@@ -24,4 +25,4 @@ def mungeFile(img_path):
 if __name__ == "__main__":
     img_path = "C://Datasets/chest_xray/train/"
     train_df = mungeFile(img_path)
-    print(train_df)
+    print(train_df["image_name"][0])
